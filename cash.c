2b734d427450494f1ca1b32e1812a7d523d0bf99
Marcox132 <Marcox132@users.noreply.github.com>
2020-04-21 07:23:37 +0000

    automated commit by submit50 [lang=C.UTF-8]

---
+++ cash.c
@@ -0,0 +1,62 @@
+// Libraries
+#include <stdio.h>
+#include <cs50.h>
+
+// Variables
+int quarter = 25;
+int dime = 10;
+int nickel = 5;
+int penny = 1;
+float owedF;
+int owedI;
+int coins = 0;
+
+// Custom Functions
+void greedy();
+
+int main(void)
+{
+    // Prompts the user for float
+    do
+    {
+        owedF = get_float("Change owed: ");
+    }
+    while (owedF < 0);
+    owedI = owedF * 100;
+
+    // Custom function that does the greedy algorithm ;)
+    greedy();
+
+    // Prints the least number of coins owed using the greedy algorithm
+    printf("%i\n", coins);
+
+    // Tells that the program ran successfully
+    return 0;
+}
+
+void greedy(void)
+{
+    while (owedI > 0)
+    {
+        if (owedI >= quarter)
+        {
+            coins++;
+            owedI-=quarter;
+        }
+        else if (owedI >= dime)
+        {
+            coins++;
+            owedI-=dime;
+        }
+        else if (owedI >= nickel)
+        {
+            coins++;
+            owedI-=nickel;
+        }
+        else
+        {
+            coins++;
+            owedI-=penny;
+        }
+    }
+}
