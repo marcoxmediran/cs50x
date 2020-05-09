550e65460b46c92c1ad3dd43c6d5ffea4cc8a00b
Marcox132 <Marcox132@users.noreply.github.com>
2020-05-09 06:54:35 +0000

    automated commit by check50 [check50=True]

---
+++ helpers.c
@@ -0,0 +1,36 @@
+#include "helpers.h"
+#include <math.h>
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height - 1; i++)
+    {
+        for (int j = 0; j < width - 1; j++)
+        {
+            int ave = round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3);
+            image[i][j].rgbtRed = ave;
+            image[i][j].rgbtGreen = ave;
+            image[i][j].rgbtBlue = ave;
+        }
+    }
+    return;
+}
+
+// Convert image to sepia
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
