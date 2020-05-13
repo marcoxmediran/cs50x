57c770190fe6e14a1dbd8258eb1cad84a422a388
Marcox132 <Marcox132@users.noreply.github.com>
2020-05-13 08:00:15 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -31,6 +31,36 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Placeholder float to store RGB Values when swapping different RGB Values on two different pixels
+    float tempRed;
+    float tempGreen;
+    float tempBlue;
+    // This loops iterates over each pixel of the image then swaps each pixel's
+    // RGB Values to its mirror Pixel
+    for (int i = 0; i < height; i++)
+    {
+        // Second condition uses width divided by 2 since we move
+        // TWO pixel at each loop
+        // NOTE: Middle pixels will not be swapped since they are not in
+        // the scope of width divided by 2
+        for (int j = 0; j < width / 2; j++)
+        {
+            // Copy pixel's RGB Values to temp floats
+            tempRed = image[i][j].rgbtRed;
+            tempGreen = image[i][j].rgbtGreen;
+            tempBlue = image[i][j].rgbtBlue;
+            // Using the width variable, we can access the remaining half of the picture
+            // to swap with it's mirror pixel
+            image[i][j].rgbtRed = image[i][width - j - 1].rgbtRed;
+            image[i][j].rgbtGreen = image[i][width - j - 1].rgbtGreen;
+            image[i][j].rgbtBlue = image[i][width - j - 1].rgbtBlue;
+            // Since the other half is swaped, we alter the other half's RGB Values
+            // with our temp RGB Values
+            image[i][width - j - 1].rgbtRed = tempRed;
+            image[i][width - j - 1].rgbtGreen = tempGreen;
+            image[i][width - j - 1].rgbtBlue = tempBlue;
+        }
+    }
     return;
 }
 
