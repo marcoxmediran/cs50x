824a43b378d0240f63136c722f3e4ac9f340756c
Marcox132 <Marcox132@users.noreply.github.com>
2021-05-26 19:49:54 +0800

    automated commit by check50 [check50=True]

---
+++ population.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    // Prompt for start size
+    int start;
+    do
+    {
+        start = get_int("Start size: ");
+    }
+    while (start < 9);
+    
+    //  Prompt for end size
+    int end;
+    do
+    {
+        end = get_int("End size: ");
+    }
+    while (end < start);
+
+    // Calculate number of years until we reach threshold
+    int years = 0;
+    do
+    {
+        int toAdd = (float)(start / 3) - (float)(start / 4);
+        start += toAdd;
+        years++;
+    }
+    while (start < end);
+    // Print number of years
+    printf("Years: %i\n", years);
+
+    // Program ran successfuly
+    return 0;
+}
