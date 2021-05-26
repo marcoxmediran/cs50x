f157fc90c12f08aeebae66f972d7d3975ddf6c35
Marcox132 <Marcox132@users.noreply.github.com>
2021-05-26 16:02:31 +0800

    automated commit by submit50

--- hello.c
+++ hello.c
@@ -2,7 +2,7 @@
 #include <cs50.h>
 
 int main(void) {
-	// Get input from user then print greetings
-	string name = get_string("Name: ");
-	printf("hello, %s\n", name);
+    // Get input from user then print greetings
+    string name = get_string("Name: ");
+    printf("hello, %s\n", name);
 }
